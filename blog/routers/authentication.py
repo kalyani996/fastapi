acb6e8dce1a9adb67c3ce0ed86b3c92019c56b97
unknown <kalyaniprajapati996@gmail.com>
2025-06-26 19:13:00 -0500

    change if condition to veirfy pwd

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -14,7 +14,7 @@ def login(request:Annotated[OAuth2PasswordRequestForm,Depends()],db:Session = De
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Invalid Credentials")
-    if Hash.verify(user.password,request.password):
+    if not Hash.verify(user.password,request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Incorrect Password")
     # access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = token.create_access_token(
